d0bcd9178e5ca023c09d7d20e2e5f071d07f4419
gaotong <gaotong2055@163.com>
2017-12-14 22:58:01 +0800

    init

---
+++ Sources/Swift/FlagKit/FlagStyle.swift
@@ -0,0 +1,40 @@
+//
+//  Copyright © 2017 Bowtie. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+@objc(FKFlagStyle)
+public enum FlagStyle: Int {
+    /**
+     Rectangle of 21 x 15 points with no styling.
+    */
+    case none
+    
+    /**
+     Rectangle of 21 x 15 points with rounded corners.
+    */
+    case roundedRect
+    
+    /**
+     Square of 15 x 15 points with rounded corners.
+    */
+    case square
+    
+    /**
+     Circular flag of 15 x 15 points.
+    */
+    case circle
+    
+    
+    public var size: CGSize {
+        switch self {
+        case .none, .roundedRect:
+            return CGSize(width: 42, height: 30)
+        case .square, .circle:
+            return CGSize(width: 15, height: 15)
+        
+        }
+    }
+}
